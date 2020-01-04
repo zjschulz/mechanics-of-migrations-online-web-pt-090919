d1fab5dba2c66d139c5f3a0c4a7b69084334ad15
Zachary Schulz <zjschulz@gmail.com>
2020-01-04 16:05:12 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,18 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+
+  def up 
+  end
+
+  def down
+  end
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+end
